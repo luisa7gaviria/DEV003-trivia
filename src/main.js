55d4b517a356059bdf479cebdccd82d5c757b541
Luisa <lorenitaa889@gmail.com>
2023-02-17 19:47:33 -0500

    trivia

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
-/*
-  Aquí va tu JavaScript
-*/
+let correctas = [1,2,2];
+
+let opcion_elegida=[];
+
+let cantidad_correctas=0;
+
+function respuesta(num_pregunta,seleccionada){
+
+	opcion_elegida[num_pregunta] = seleccionada.value;
+
+	id="p" + num_pregunta;
+
+	labels = document.getElementById(id).childNodes;
+	
+	
+	
+}
+function MostrarResultado(){
+	cantidad_correctas = 0;
+	for(i=0;i <correctas.length;i++){
+		if(correctas[i]==opcion_elegida[i]){
+			cantidad_correctas++;
+		}
+	}
+	document.getElementById("resultado").innerHTML=cantidad_correctas;
+}
